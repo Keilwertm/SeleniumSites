64e8c513bb61494d57aec7aa8e644311aaafafa3
Mel Keilwert <keilwertmelg@gmail.com>
2025-04-08 15:28:50 -0600

    Initial Commit

---
+++ UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace SeleniumStuff;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
